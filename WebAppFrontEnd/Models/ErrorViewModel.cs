dfd52cbd939dc5bdcf9482ee3c78861dfa543086
DildaarYounglings <dildaar.abrahams@younglings.africa>
2024-08-04 15:59:03 +0200

    first commit

---
+++ WebAppFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebAppFrontEnd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
